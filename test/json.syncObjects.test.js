48494143bb0408e2f666943bd646649eafb5b9ed
Monica Vrancean <monica.vrancean@evozon.com>
2015-10-06 11:27:47 +0300

    Modified syncObjects test

--- test/json.syncObjects.test.js
+++ test/json.syncObjects.test.js
@@ -7,17 +7,30 @@ describe('JSON', function() {
             var originalJson = {
                 user_name: 'Andrei',
                 user: 'Andrei',
-                email_addr: 'andrei@irongwing'
+                email_addr: 'andrei@irongwing',
+                address: {
+                    home_city: 'Brasov',
+                    actual_city: 'Cluj-Napoca',
+                    actual_country: 'Romania'
+                }
             };
             var camelCasedJson = {
                 userName: 'MihaiAndrei',
                 user: 'Andrei',
-                emailAddr: 'mihai.andrei@irongwing'
+                emailAddr: 'mihai.andrei@irongwing',
+                address: {
+                    homeCity: 'Bucuresti',
+                    actualCity: 'Sibiu',
+                    actualCountry: 'Romania'
+                }
             };
             syncedJSON = iwUtils.syncObjects(originalJson, camelCasedJson);
 
             assert.equal(syncedJSON.user_name, 'MihaiAndrei');
             assert.equal(syncedJSON.email_addr, 'mihai.andrei@irongwing');
+            assert.equal(syncedJSON.address.home_city, 'Bucuresti');
+            assert.equal(syncedJSON.address.actual_city, 'Sibiu');
+            assert.equal(syncedJSON.address.actual_country, 'Romania');
         });
     });
 });
