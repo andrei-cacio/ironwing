b00599d6096c074d269c9f45d4fe6fa866bf83fd
Andrei Cacio <andrei.cacio@yahoo.com>
2015-10-11 14:17:08 +0300

    Added new tests

--- test/json.post.json.test.js
+++ test/json.post.json.test.js
@@ -4,15 +4,35 @@ var assert = require('assert'),
     postJson = require('../demo/api/post');
 
 describe('JSON', function() {
-  describe('#testong on post.json', function() {
+  describe('#testing on post.json', function() {
     it('should work with large JSON objects', function() {
-      var cameledPostCloned =  iwUtils.toCamel(clone(postJson, true));
+      var cameledPostCloned =  iwUtils.toCamel(clone(postJson, true)),
+          syncedObject;
 
       cameledPostCloned.processedCover = 'test.jpg';
+      cameledPostCloned.blog.userBlog.blog.domainChangeCount = 100;
+      cameledPostCloned.blog.userBlog.onepager.processedCover = 'img.jpg';
+
+      syncedObject = iwUtils.syncObjects(postJson, cameledPostCloned);
+
+      assert.equal(syncedObject.processed_cover, 'test.jpg');
+      assert.equal(syncedObject.blog.user_blog.blog.domain_change_count, 100);
+      assert.equal(syncedObject.blog.user_blog.onepager.processed_cover, 'img.jpg');
+    });
+    it('should work with arrays inside JSON', function() {
+      var cameledPostCloned =  iwUtils.toCamel(clone(postJson, true));
+
+      cameledPostCloned.categories[0].name = 'Linux';
 
       var syncedObject = iwUtils.syncObjects(postJson, cameledPostCloned);
 
-      assert.equal(syncedObject.processed_cover, 'test.jpg');
+      assert.equal(syncedObject.categories[0].name, 'Linux');
+    });
+    it('sync should not affect object structure', function() {
+      var cameledPostCloned =  iwUtils.toCamel(clone(postJson, true)),
+          syncedObject = iwUtils.syncObjects(postJson, cameledPostCloned);
+
+      assert.equal(JSON.stringify(postJson), JSON.stringify(syncedObject));
     });
   });
 });
